3220e67a64d4a493f4ce4d7511d1b84c83b60efa
Arthur-Rib <arthurbrito2003@gmail.com>
2021-12-01 23:42:49 +0000

    criação da unidade de teste correta

---
+++ calculadora_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_soma(t *testing.T) {
+	type args struct {
+		x float64
+		y float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := soma(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("soma() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
